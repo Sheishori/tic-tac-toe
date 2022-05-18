8a18463cef62d453d61c6d8e6f271050a5357354
Sheishori <sheishori@gmail.com>
2022-05-18 10:48:05 +0200

    Populate the html and generate a game board

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
 const game = (() => {
 	let	gameBoard = [];
 	
+	displayController();
+
 	const player = (name) => {
 		 return {name};
 	};
 	
-	const displayController = {
-
+	function displayController() {
+		let display = document.querySelector("#game");
+		for (i = 0; i < 9; i++) {
+			let square = document.createElement("div");
+			square.classList.add("square");
+			display.append(square);
+		}
 	};
 
 	return {gameBoard};
 })();
+
