4b89879c246ee65f2da95577d4074c4d46d3353a
Sheishori <sheishori@gmail.com>
2022-05-18 12:38:33 +0200

    Allow marking fields on the board

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const game = (() => {
 	let	gameBoard = [];
-	
+	let counter = 0;
+
 	displayController();
 
 	const player = (name) => {
@@ -9,12 +10,33 @@ const game = (() => {
 	
 	function displayController() {
 		let display = document.querySelector("#game");
+		display.textContent = "";
 		for (i = 0; i < 9; i++) {
 			let square = document.createElement("div");
 			square.classList.add("square");
+			square.classList.add(i);
+			square.textContent = gameBoard[i];
 			display.append(square);
 		}
+		playerMove();
 	};
+
+	function playerMove() {
+		let fields = document.querySelectorAll(".square");
+		fields.forEach(element => {
+			element.addEventListener("click", () => {
+				if (counter === 0) {
+					gameBoard[element.classList[1]] = "X";
+					counter = 1;
+				} else {
+					gameBoard[element.classList[1]] = "O";
+					counter = 0;
+				}
+				displayController();
+			})
+		});
+	}
+
 
 	return {gameBoard};
 })();
