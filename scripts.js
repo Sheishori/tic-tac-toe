c0dc29cf8f97f63f443809701d0fbe43853db69a
Sheishori <sheishori@gmail.com>
2022-05-18 13:37:06 +0200

    Add game logic

--- scripts.js
+++ scripts.js
@@ -34,9 +34,25 @@ const game = (() => {
 						counter = 0;
 					}
 					displayController();
+					checkResult();
 				}
 			})
 		});
-	}
+	};
+
+	function checkResult() {
+		let winningConditions = [];
+		winningConditions[0] = (gameBoard[0] !== undefined && gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2]) ? gameBoard[0] : undefined;
+		winningConditions[1] = (gameBoard[3] !== undefined && gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5]) ? gameBoard[3] : undefined;
+		winningConditions[2] = (gameBoard[6] !== undefined && gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8]) ? gameBoard[6] : undefined;
+		winningConditions[3] = (gameBoard[0] !== undefined && gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6]) ? gameBoard[0] : undefined;
+		winningConditions[4] = (gameBoard[1] !== undefined && gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7]) ? gameBoard[1] : undefined;
+		winningConditions[5] = (gameBoard[2] !== undefined && gameBoard[2] === gameBoard[5] && gameBoard[2] === gameBoard[8]) ? gameBoard[2] : undefined;
+		winningConditions[6] = (gameBoard[0] !== undefined && gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8]) ? gameBoard[0] : undefined;
+		winningConditions[7] = (gameBoard[2] !== undefined && gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6]) ? gameBoard[2] : undefined;
+		if (winningConditions.find(condition => {
+			if (condition == "X" || condition == "O") alert(condition + " won!");
+		}));
+	};
 })();
 
