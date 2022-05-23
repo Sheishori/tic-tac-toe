4680e7876a3ca99e840b5d06cd996889896c5ae9
Sheishori <sheishori@gmail.com>
2022-05-23 12:52:06 +0200

    Tuck game functions inside internal modules

--- scripts.js
+++ scripts.js
@@ -1,64 +1,101 @@
-const game = (() => {
-	let	gameBoard = [];
-	let counter = 0;
-	const boardDisplay = document.querySelector("#game");
+const ticTacToe = (() => {
+	const gameBoard = (() => {
+		let	board = [];
+		
+		function getBoard() {
+			return board;
+		}
 
-	displayController();
+		function updateBoard(newBoard) {
+			board = newBoard;
+		}
 
-	const player = (name) => {
-		 return {name};
+		return {getBoard, updateBoard}
+	})();
+
+
+	const player = (name, marker) => {
+		function playerMove() {
+			let board = gameBoard.getBoard();
+			if (board[this.classList[1]] === undefined) {
+				board[this.classList[1]] = game.getCurrentPlayer().marker;
+				gameBoard.updateBoard(board);
+				displayController.render();
+				game.checkResult();
+				game.endTurn();
+			}
+		};
+
+		return {name, marker, playerMove};
 	};
 	
-	function displayController() {
-		createBoard();
-		bindEvents();
-	};
 
-	function createBoard() {
-		boardDisplay.textContent = "";
-		for (i = 0; i < 9; i++) {
-			let square = document.createElement("div");
-			square.classList.add("square");
-			square.classList.add(i);
-			square.textContent = gameBoard[i];
-			boardDisplay.append(square);
-		}
-	}
+	const game = (() => {
+		let playerTurn = 1;
+		
+		function getCurrentPlayer() {
+			return (playerTurn === 1) ? player1 : player2;
+		};
 
-	function bindEvents() {
-		let fields = boardDisplay.querySelectorAll(".square");
-		fields.forEach(element => {
-			element.addEventListener("click", playerMove);
-		});
-	};
+		function endTurn() {
+			playerTurn = (playerTurn === 1) ? 2 : 1;
+		};
 
-	function playerMove() {
-		if (gameBoard[this.classList[1]] === undefined) {
-			if (counter === 0) {
-				gameBoard[this.classList[1]] = "X";
-				counter = 1;
-			} else {
-				gameBoard[this.classList[1]] = "O";
-				counter = 0;
-			}
-			displayController();
-			checkResult();
-		}
-	};
+		function checkResult() {
+			let winConditions = [];
+			let board = gameBoard.getBoard();
+			winConditions[0] = (board[0] !== undefined && board[0] === board[1] && board[0] === board[2]) ? board[0] : undefined;
+			winConditions[1] = (board[3] !== undefined && board[3] === board[4] && board[3] === board[5]) ? board[3] : undefined;
+			winConditions[2] = (board[6] !== undefined && board[6] === board[7] && board[6] === board[8]) ? board[6] : undefined;
+			winConditions[3] = (board[0] !== undefined && board[0] === board[3] && board[0] === board[6]) ? board[0] : undefined;
+			winConditions[4] = (board[1] !== undefined && board[1] === board[4] && board[1] === board[7]) ? board[1] : undefined;
+			winConditions[5] = (board[2] !== undefined && board[2] === board[5] && board[2] === board[8]) ? board[2] : undefined;
+			winConditions[6] = (board[0] !== undefined && board[0] === board[4] && board[0] === board[8]) ? board[0] : undefined;
+			winConditions[7] = (board[2] !== undefined && board[2] === board[4] && board[2] === board[6]) ? board[2] : undefined;
+			if (winConditions.find(condition => {
+				if (condition == "X" || condition == "O") alert(condition + " won!");
+			}));
+		};
+		return {getCurrentPlayer, endTurn, checkResult};
+	})();
 
-	function checkResult() {
-		let winningConditions = [];
-		winningConditions[0] = (gameBoard[0] !== undefined && gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2]) ? gameBoard[0] : undefined;
-		winningConditions[1] = (gameBoard[3] !== undefined && gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5]) ? gameBoard[3] : undefined;
-		winningConditions[2] = (gameBoard[6] !== undefined && gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8]) ? gameBoard[6] : undefined;
-		winningConditions[3] = (gameBoard[0] !== undefined && gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6]) ? gameBoard[0] : undefined;
-		winningConditions[4] = (gameBoard[1] !== undefined && gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7]) ? gameBoard[1] : undefined;
-		winningConditions[5] = (gameBoard[2] !== undefined && gameBoard[2] === gameBoard[5] && gameBoard[2] === gameBoard[8]) ? gameBoard[2] : undefined;
-		winningConditions[6] = (gameBoard[0] !== undefined && gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8]) ? gameBoard[0] : undefined;
-		winningConditions[7] = (gameBoard[2] !== undefined && gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6]) ? gameBoard[2] : undefined;
-		if (winningConditions.find(condition => {
-			if (condition == "X" || condition == "O") alert(condition + " won!");
-		}));
-	};
+
+	const displayController = (() => {
+		const boardDisplay = document.querySelector("#game");
+
+		function render() {
+			_clearBoard();
+			_createBoard();
+			_bindEvents();
+		};
+
+		function _createBoard() {
+			for (i = 0; i < 9; i++) {
+				let square = document.createElement("div");
+				square.classList.add("square");
+				square.classList.add(i);
+				square.textContent = gameBoard.getBoard()[i];
+				boardDisplay.append(square);
+			};
+		};
+
+		function _bindEvents() {
+			let fields = boardDisplay.querySelectorAll(".square");
+			fields.forEach(element => {
+				element.addEventListener("click", game.getCurrentPlayer().playerMove);
+			});
+		};
+
+		function _clearBoard() {
+			boardDisplay.textContent = "";
+		};
+
+		return {render};
+	})();
+
+
+	const player1 = player("first", "X");
+	const player2 = player("second", "O");
+	displayController.render();
 })();
 
