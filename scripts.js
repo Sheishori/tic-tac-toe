b3b94edb4bd1741addc8b6c0e34eabb4a5de7de1
Sheishori <sheishori@gmail.com>
2022-05-23 10:56:16 +0200

    Move some code to separate functions

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const game = (() => {
 	let	gameBoard = [];
 	let counter = 0;
+	const boardDisplay = document.querySelector("#game");
 
 	displayController();
 
@@ -9,35 +10,40 @@ const game = (() => {
 	};
 	
 	function displayController() {
-		let display = document.querySelector("#game");
-		display.textContent = "";
+		createBoard();
+		bindEvents();
+	};
+
+	function createBoard() {
+		boardDisplay.textContent = "";
 		for (i = 0; i < 9; i++) {
 			let square = document.createElement("div");
 			square.classList.add("square");
 			square.classList.add(i);
 			square.textContent = gameBoard[i];
-			display.append(square);
+			boardDisplay.append(square);
 		}
-		playerMove();
+	}
+
+	function bindEvents() {
+		let fields = document.querySelectorAll(".square");
+		fields.forEach(element => {
+			element.addEventListener("click", playerMove);
+		});
 	};
 
 	function playerMove() {
-		let fields = document.querySelectorAll(".square");
-		fields.forEach(element => {
-			element.addEventListener("click", () => {
-				if (gameBoard[element.classList[1]] === undefined) {
-					if (counter === 0) {
-						gameBoard[element.classList[1]] = "X";
-						counter = 1;
-					} else {
-						gameBoard[element.classList[1]] = "O";
-						counter = 0;
-					}
-					displayController();
-					checkResult();
-				}
-			})
-		});
+		if (gameBoard[this.classList[1]] === undefined) {
+			if (counter === 0) {
+				gameBoard[this.classList[1]] = "X";
+				counter = 1;
+			} else {
+				gameBoard[this.classList[1]] = "O";
+				counter = 0;
+			}
+			displayController();
+			checkResult();
+		}
 	};
 
 	function checkResult() {
